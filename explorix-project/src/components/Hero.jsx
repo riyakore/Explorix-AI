18d7cf872f675e6a42d6ecc3d5ecf14a80b45c84
Riya Kore <rykore@wisc.edu>
2024-07-02 02:22:02 +0530

    Update Hero.jsx

--- explorix-project/src/components/Hero.jsx
+++ explorix-project/src/components/Hero.jsx
@@ -41,11 +41,9 @@ const Hero = () => {
           <p className="body-1 max-w-3xl mx-auto mb-6 text-cyan-500 lg:mb-8">
             Make your customized travel itinerary with us!
           </p>
-          <Router>
-            <Button className={`hover:text-color-7`} href="/Customize">
-              Start Here!
-            </Button>
-          </Router>
+          <Button className={`hover:text-color-7`} href="/Customize">
+            Start Here!
+          </Button>
         </div>
         <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
           <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
