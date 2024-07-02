900fc0d32b8673ead0f365ba0dd59960e49d0034
Riya Kore <rykore@wisc.edu>
2024-07-02 14:41:33 +0530

    Update App.jsx

--- explorix-project/src/App.jsx
+++ explorix-project/src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 import Roadmap from "./components/Roadmap";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
   return (
